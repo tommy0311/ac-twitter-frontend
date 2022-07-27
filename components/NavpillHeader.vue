0eb8a130a79989bbd2a321a4949bfbd6e65d91e9
eddie8119 <wang8119@gmail.com>
2022-07-28 01:31:50 +0800

    Delete components directory
    之前備份的很原始檔案刪除

--- components/NavpillHeader.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div id="navpillHeader-container">
-    <img class="arrow-icon d-none" src="../assets/arrow.png" alt="回到上一頁" />
-    <div class="navpill-info-container flex-column">
-      <a class="navpillHeader-title">首頁</a>
-      <p class="post-number-hint d-none">25<span>推文</span></p>
-    </div>
-  </div>
-</template>
