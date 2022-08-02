90285a4091ab6e395448f5eac5932dfea044fa41
tommy0311 <chen.li.tang@gmail.com>
2022-08-02 18:07:07 +0800

    chore: add process.env.VUE_APP_BASE_URL (#34)
    [T05] chore: add process.env.VUE_APP_BASE_URL #34

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import Swal from "sweetalert2";
+import process from "process"
 
-const baseURL = "http://localhost:3000/api";
-// const baseURL = "https://twitterac.herokuapp.com/api";
+const baseURL = process.env.VUE_APP_BASE_URL || "https://twitterac.herokuapp.com/api";
 
 const axiosInstance = axios.create({
   baseURL
