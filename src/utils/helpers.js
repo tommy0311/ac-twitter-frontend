1dec0786b607ce84b095503419eb7136fa81816a
TommyChen <chen.li.tang@gmail.com>
2022-10-03 19:57:21 +0800

    chore: change api server url

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Swal from "sweetalert2";
 import process from "process";
-const baseURL = process.env.VUE_APP_BASE_URL || "https://twitterac.herokuapp.com/api";
+const baseURL = process.env.VUE_APP_BASE_URL || "https://txgychuvbf.us12.qoddiapp.com/api";
 
 const axiosInstance = axios.create({
   baseURL,
