f49f68c313d2db5ac286bea8fb76db89c14b572f
TommyChen <chen.li.tang@gmail.com>
2022-10-04 15:07:20 +0800

    chore: deploy to github pages

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,34 @@
 const { defineConfig } = require('@vue/cli-service')
+const serverInDev = {
+  allowedHosts: "all",
+  client: {
+    webSocketURL: {
+      hostname: 'appweb.asuscomm.com',
+      pathname: '/ws',
+      password: 'dev-server',
+      port: 28080,
+      protocol: 'ws',
+      username: 'webpack',
+    }
+  }
+}
+
+const serverInGithub = {
+  allowedHosts: "all",
+  client: {
+    webSocketURL: {
+      hostname: 'tommy0311.github.io',
+      pathname: '/ws',
+      password: 'dev-server',
+      port: 8080,
+      protocol: 'ws',
+      username: 'webpack',
+    }
+  }
+}
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? 'ac-twitter-frontend'
-    : '/'
+  publicPath: '/ac-twitter-frontend/',
+  devServer: process.env.NODE_ENV === 'development' ? serverInDev : serverInGithub
 })
